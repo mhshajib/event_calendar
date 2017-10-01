787e7b54555129cf4771f0d290d6b0955c4acad8
mhshajib <shajib0513@gmail.com>
2017-10-01 17:37:51 +0600

    made test case for creating an event

--- test/event.js
+++ test/event.js
@@ -29,3 +29,31 @@ describe('/GET events', function() {
             });
     });
 });
+
+describe('/POST event', function() {
+    it('it should create a new event', function(done) {
+        chai.request(server)
+            .post('/events/create')
+            .send({'year': 2017, 'month': 10, 'date': 4, "weeksIndex": 0, "dayIndex": 3, "title": "Test Event", "description": "Test Event Description" })
+            .end(function(err, res){
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('status');
+                res.body.should.have.property('message');
+                res.body.should.have.property('data');
+                res.body.data.should.be.a('object');
+                res.body.data.should.have.property('_id');
+                res.body.data.should.have.property('year');
+                res.body.data.should.have.property('month');
+                res.body.data.should.have.property('date');
+                res.body.data.should.have.property('weeksIndex');
+                res.body.data.should.have.property('dayIndex');
+                res.body.data.should.have.property('title');
+                res.body.data.should.have.property('description');
+                res.body.data.title.should.equal('Test Event');
+                res.body.data.description.should.equal('Test Event Description');
+                done();
+            });
+    });
+});
